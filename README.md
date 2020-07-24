b7fa4489a24b2fcd816065100f4030ec8bcc0b5a
Lauri Neuvonen <lauri.neuvonen@aalto.fi>
2020-07-24 19:33:28 +0300

    Started implementing policy optimization model in pymoo (own code in policy_optimizer.py). Next: finish implementation for first optimization trial, then expand to real cases. Added SEIR model to be used with policy optimization into separate file (policy_epidemic_model_code.py). The non-optimizable epidemic model separated into model_code.py. README updated.

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
 # Policy optimization for Covid with imperfect testing
 
 
+MIT license
+copyright Lauri Neuvonen, July 2020
 
-Based on original paper by Berger, Herkenhoff & Mongey:
+## General information
 
+WORK IN PROGRESS: this work has not been published or finished yet! Absolutely no warranty for results or code.
+
+Epidemic modeling based on original paper by Berger, Herkenhoff & Mongey:
 http://www.simonmongey.com/uploads/6/5/6/6/65665741/bhm_corona_v6.pdf
 
-- Work still in progress.
+
+## Optimization model & optimizable epidemic model:
+
+This model set will allow optimization of epidemic mitigation policies. NOT IN WORKING ORDER YET! (Jul 24, 2020)
+
+Files:
+- policy_epidemic_model_code.py
+- policy_optimizer.py
+
+## Epidemic model without optimization:
+
+This model can be used to simulate different testing scenarios (vary testing rate, sensitivity, specificity)
+
+Files:
+- model_code.py
+- Covid_Imperfect_Testing.ipynb
+
+
+## Utilities:
+Files: 
+- jupyterWidgets.py - used in jupyter notebooks to allow parameter setting
--- policy_optimizer.py
+++ policy_optimizer.py
@@ -9,24 +9,69 @@ import numpy as np
 from pymoo.util.misc import stack
 from pymoo.model.problem import Problem
 
-class MyProblem(Problem):
+from pymoo.algorithms.nsga2 import NSGA2
+from pymoo.factory import get_sampling, get_crossover, get_mutation
 
-    def __init__(self):
-        super().__init__(n_var=2,
+from pymoo.factory import get_termination
+
+from policy_epidemic_model_code import *
+
+termination = get_termination("n_gen", 40)
+
+ξ_base = 1.0
+A_rel = 0.5
+r_AP = 0.0
+d_vaccine = 500
+rel_ρ = 1.0
+δ_param = 6
+ωR_param = 14
+π_D =  0.01
+R_0 = 2.0
+rel_λ = 0.5
+initial_infect = 300
+
+corona_model = optimizable_corona_model(ξ_base, A_rel, r_AP, d_vaccine, rel_ρ, δ_param, \
+                 ωR_param, π_D, R_0, rel_λ,initial_infect)
+
+policy_control_days = [15, 30, 45, 100]
+
+class COVID_policy(Problem):
+
+    def __init__(self, model, policy_control_days):
+
+        self.model = model
+        self.policy_control_days = policy_control_days
+
+        super().__init__(n_var=len(policy_control_days),
                          n_obj=2,
-                         n_constr=2,
-                         xl=np.array([-2,-2]),
-                         xu=np.array([2,2]))
+                         n_constr=0,
+                         xl=np.array([0,0]),
+                         xu=np.array([1,1]))
 
     def _evaluate(self, x, out, *args, **kwargs):
-        f1 = x[:,0]**2 + x[:,1]**2
-        f2 = (x[:,0]-1)**2 + x[:,1]**2
 
-        g1 = 2*(x[:, 0]-0.1) * (x[:, 0]-0.9) / 0.18
-        g2 = - 20*(x[:, 0]-0.4) * (x[:, 0]-0.6) / 4.8
+        for i in len(x[:,1]):
+            # TODO:  implement calculation of f1, f2 for all individuals (represented by rows in x)
+            policy_control_levels = x[i,:]
+
+
+            policy = {}
+            for (i, t) in enumerate(policy_control_days):
+                policy[t] = x[:,i]
+
+        Reported_D, Notinfected_D, Unreported_D, Infected_D, \
+        False_pos, False_neg, Recovered_D, Dead_D, Infected_T, Infected_not_Q, Infected_in_Q, Y_D, M_t, Y_total \
+        = self.model.solve_policy_case(self.model.baseline, policy)
+        ## For reference - TODO delete when not needed:
+        #f1 = x[:,0]**2 + x[:,1]**2
+        #f2 = (x[:,0]-1)**2 + x[:,1]**2
+
+        #g1 = 2*(x[:, 0]-0.1) * (x[:, 0]-0.9) / 0.18
+        #g2 = - 20*(x[:, 0]-0.4) * (x[:, 0]-0.6) / 4.8
+
 
         out["F"] = np.column_stack([f1, f2])
-        out["G"] = np.column_stack([g1, g2])
+        #out["G"] = np.column_stack([g1, g2])
 
 
     # --------------------------------------------------
@@ -54,3 +99,20 @@ class MyProblem(Problem):
         return stack(a,b, flatten=flatten)
 
 problem = MyProblem()
+
+algorithm = NSGA2(
+    pop_size=40,
+    n_offsprings=10,
+    sampling=get_sampling("real_random"),
+    crossover=get_crossover("real_sbx", prob=0.9, eta=15),
+    mutation=get_mutation("real_pm", eta=20),
+    eliminate_duplicates=True
+)
+
+res = minimize(problem,
+               algorithm,
+               termination,
+               seed=1,
+               pf=problem.pareto_front(use_cache=False),
+               save_history=True,
+               verbose=True)
