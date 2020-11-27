9bbeec657de81b9e2673272386b7e5ec3d26d02b
Lauri Neuvonen <lauri.neuvonen@aalto.fi>
2020-11-27 20:08:39 +0200

    Adds CVaR calculation for samples derived from Pareto front policies and parameter distributions. This can be used to prune the Pareto front given some kind of risk criteria. Policy specific sample results are saved into .csv files named by run and policy number.

--- risk_analysis.py
+++ risk_analysis.py
@@ -1,14 +1,19 @@
 import policy_epidemic_model_code
 import numpy as np
+import pandas as pd
 
-from run_tools import create_epidemic_model, create_policy_control, Policy, Policy_template
+from run_tools import create_epidemic_model, create_sub_policy, create_policy, Policy, Policy_template
 from run_definitions import get_runs_definitions
+from math import floor
 import argparse
 from run_definitions import (ksi_base_default, A_rel_default, r_AP_default, r_U_default, r_P_default, r_N_default, d_vaccine_default, rel_rho_default, delta_param_default,
     omegaR_param_default, pii_D_default, R_0_default, rel_lambda_param_default, gamma_param_default, initial_infect_default, daily_testing_rate_default,
     testing_sensitivity_default, testing_specificity_default, tau_TT_daily_default, eta_default, unknown_q_rate_default, recovered_q_rate_default,
     negative_q_rate_default, positive_q_rate_default, testing_cost_default)
 
+# command line inputs
+# first argument = sample size, integer
+# second argument = runs to be analyzed, run names as strings, e.g. 'romer' 'romer_R0_4.0' ...
 parser = argparse.ArgumentParser(description='Run optimization run using an NSGA-II algorithm for COVID-19 simulator')
 parser.add_argument('sample_size', type=int, help='sample size for risk analysis')
 parser.add_argument('runs', type=str, nargs='+', help='dictionary of run values to be changed from defaults')
@@ -21,20 +26,42 @@ run_definitions = get_runs_definitions()
 
 runs = { run: run_definitions[run] for run in run_list } # filters the correct run definitions based on given run list
 
-### INPUT RISK ANALYSIS DEFINITIONS HERE! ###
-### MAKE SURE CORRECT SOURCE FOR EXP is used ###
+
+def sample_CVaR(sample, alpha, lowest_alpha=True):
+
+    # this function calculates a CVaR -type 'worst case' expectation value for event mass corresponding to alpha
+    # e.g. of sample group of N=100, this selects 10 worst values and calculates their average
+    # lowest_alpha controls if the alpha share is selected from the low end (or the high end) of sample. If true the
+    # lowest alpha_N values are used.
+
+    sample_size = len(sample)
+    print("sample size:", sample_size)
+    print("alpha*sample size: ", sample_size*alpha, " --> ", floor(sample_size*alpha))
+    alpha_N = floor(sample_size*alpha) # number of cases corresponding to share alpha
+
+    sample.sort(reverse=lowest_alpha)
+    worst_alpha = sample[-alpha_N:]
+
+    remainder = (sample_size*alpha - alpha_N)*sample[-(alpha_N+1)]
+    print("worst alpha: ", worst_alpha, " remainder: ", remainder, 'from', sample[-(alpha_N+1)])
+
+    return (sum(worst_alpha)+remainder)/(sample_size*alpha)
 
 for run in runs:
 
     # get policies:
 
     run_policies_df = pd.read_csv('active_results/' + run + '_results.csv', delimiter=',')
+    run_control_times = list(map(int, run_policies_df.columns))
     run_policies = run_policies_df.to_numpy()
 
 
     # get parameter value samples:
     print("run: ", runs[run])
     sample_list = []
+
+    ### INPUT RISK ANALYSIS DEFINITIONS BELOW! ###
+    ### MAKE SURE CORRECT SOURCE FOR EXP is used ###
 
     analysis_params = {}
 
@@ -64,16 +91,71 @@ for run in runs:
    #debug
     print("samples: ", sample_list)
 
-    for sample in sample_list:
-        sample_run_params = runs[run].copy() # copies the original run (e.g. 'romer') for updating with sample values
-        sample_run_params.update(sample)
-        #debug:
-        print("sample params: ", sample_run_params)
 
-        model, model_case = create_epidemic_model(sample_run_params)
-        policy = create_policy_control() # how was this done...
+    run_policy_samples = {}
 
+    for policy_id, policy in enumerate(run_policies):
 
+        # create policy for run:
+
+        try:
+            lockdown_only = (runs[run]['testing_policy_control_days'] == "NA")
+        except:
+            lockdown_only = False  # if testing not defined in the run, default used -> testing used
+
+        try:
+            testing_only = (runs[run]['lockdown_policy_control_days'] == "NA")
+        except:
+            testing_only = False
+
+        if lockdown_only:
+            ld_policy = create_sub_policy(run_control_times, policy)
+            test_policy = "NA"
+
+        elif testing_only:
+            test_policy = create_sub_policy(run_control_times, policy)
+            ld_policy = "NA"
+
+        else:
+            ld_control_times = run_control_times[:len(run_control_times)//2]
+            ld_policy = create_sub_policy(ld_control_times, policy[:len(ld_control_times)])
+
+            test_control_times = run_control_times[len(run_control_times)//2:]
+            test_policy = create_sub_policy(test_control_times, policy[len(ld_control_times):])
+
+        run_policy = create_policy(ld_policy, test_policy)
+
+        policy_result_dist = {}
+        policy_sample_deaths = []
+
+        for sample_id, sample in enumerate(sample_list):
+            sample_run_params = runs[run].copy() # copies the original run (e.g. 'romer') for updating with sample values
+            sample_run_params.update(sample)
+            #debug:
+            #print("sample params: ", sample_run_params)
+
+            # calculate results
+
+            epidemic_simulator = create_epidemic_model(**sample_run_params)
+            Reported_D, Notinfected_D, Unreported_D, Infected_D, \
+            False_pos, False_neg, Recovered_D, Dead_D, Infected_T, Infected_not_Q, Infected_in_Q, Y_D, M_t, Y_total, total_testing_cost, tests, Unk_NA_nQ_D, Unk_NA_Q_D, K_NA_nQ_D, Unk_IA_nQ_D, Unk_IA_Q_D, K_IA_Q_D, alpha_D, ksi_TT_I_D, ksi_TT_N_D, ksi_TT_R_D, Symptomatic_T \
+                = epidemic_simulator[0].solve_case(epidemic_simulator[1], run_policy)
+
+            #debug
+            #print("simu deaths: ", Dead_D[-1], " | simu output: ", Y_total)
+
+            policy_result_dist[sample_id] = [Dead_D[-1], Y_total]
+            policy_sample_deaths.append(Dead_D[-1])
+
+        print("deaths in sample: ", policy_sample_deaths)
+        print("policy CVaR 10%: ", 340000000*sample_CVaR(policy_sample_deaths, 0.1, False))
+
+        #run_policy_samples[sample_id] = policy_result_dist
+
+        # saving results to csv files
+        df = pd.DataFrame(policy_result_dist, index=['Deaths', 'Output'])
+
+        df.to_csv('active_results/risk_analysis/'+run+'__'+str(policy_id)+'.csv')
 
 
 
--- run_tools.py
+++ run_tools.py
@@ -51,7 +51,7 @@ def create_epidemic_model(ksi_base=ksi_base_default,
                negative_q_rate=negative_q_rate_default,
                positive_q_rate=positive_q_rate_default,
                testing_cost=testing_cost_default,
-               ):
+               **kwargs):
 
     model = optimizable_corona_model(ksi_base, A_rel, r_AP, d_vaccine, rel_rho, delta_param, \
                                      omegaR_param, pii_D, R_0, rel_lambda_param, initial_infect, testing_cost, eta, gamma_param)
@@ -92,3 +92,92 @@ def create_policy_control(lockdown_policy_control_days=[1, 15, 30, 60, 90, 120, 
 
     return policy_control
 
+
+# NOTE: default values for all adjustable run parameters defined in function definition below:
+def create_simu_run(ksi_base=ksi_base_default,
+               A_rel=A_rel_default,
+               r_AP=r_AP_default,
+               r_U=r_U_default,
+               r_P=r_P_default,
+               r_N=r_N_default,
+               d_vaccine=d_vaccine_default, # this is in _time steps_, not in days (days would be better)
+               rel_rho=rel_rho_default,
+               delta_param=delta_param_default,
+               omegaR_param=omegaR_param_default,
+               pii_D=pii_D_default,
+               R_0=R_0_default,
+               rel_lambda_param=rel_lambda_param_default,
+               gamma_param=gamma_param_default,
+               initial_infect=initial_infect_default,
+               daily_testing_rate=daily_testing_rate_default,
+               testing_sensitivity=testing_sensitivity_default,
+               testing_specificity=testing_specificity_default,
+               tau_TT_daily=tau_TT_daily_default,
+               eta=eta_default,
+               unknown_q_rate=unknown_q_rate_default,
+               recovered_q_rate=recovered_q_rate_default,
+               negative_q_rate=negative_q_rate_default,
+               positive_q_rate=positive_q_rate_default,
+               testing_cost=testing_cost_default,
+                    pop_size=60,
+                    n_offsprings=30,
+                    # sampling=get_sampling("real_random"),
+                    # crossover=get_crossover("real_sbx", prob=0.9, eta=10),
+                    # mutation=get_mutation("real_pm", eta=8),
+                    # termination = get_termination("n_gen",50),
+
+                    lockdown_policy_control_days=[1, 15, 30, 60, 90, 120, 150, 200, 250, 300, 350, 400, 450, 500, 600],
+                    lockdown_policy_lower_limits=list(0.5 * np.ones(15)),
+                    # can't use len(l_p_c_d) within function param def
+                    lockdown_policy_upper_limits=list(1.0 * np.ones(15)),  # needs to be different from lower limit
+                    testing_policy_control_days=[1, 15, 30, 60, 90, 120, 150, 200, 250, 300, 350, 400, 450, 500, 600],
+                    testing_policy_lower_limits=list(np.zeros(15)),
+                    testing_policy_upper_limits=list(0.02 * np.ones(15)),
+                    max_daily_tests=10000000,
+                    p_ICU=0.01,
+                    C_hos=100000,
+                    T_rec=0.5  # recovery time in years from end of experiment
+                    ):
+    model = optimizable_corona_model(ksi_base, A_rel, r_AP, d_vaccine, rel_rho, delta_param, \
+                                     omegaR_param, pii_D, R_0, rel_lambda_param, initial_infect, testing_cost, eta,
+                                     gamma_param)
+
+    model_case = {
+        'tau_paramA': (1 + daily_testing_rate) ** (1. / model.Delta_time) - 1,
+        'test_sens': testing_sensitivity,
+        'test_spec': testing_specificity,
+        'tau_TT': (1 + tau_TT_daily) ** (1. / model.Delta_time) - 1,
+        'ksi_U': (1 + unknown_q_rate) ** (1. / model.Delta_time) - 1,
+        'ksi_P': (1 + positive_q_rate) ** (1. / model.Delta_time) - 1,
+        'ksi_N': (1 + negative_q_rate) ** (1. / model.Delta_time) - 1,
+        'ksi_R': (1 + recovered_q_rate) ** (1. / model.Delta_time) - 1,
+        'r_U': (1 + r_U) ** (1. / model.Delta_time) - 1,
+        'r_P': (1 + r_P) ** (1. / model.Delta_time) - 1,
+        'r_AP': 0.0,
+        'r_N': (1 + r_N) ** (1. / model.Delta_time) - 1,
+        'r_R': (1 + r_N) ** (1. / model.Delta_time) - 1,  # same rate as for negatives
+        'd_start_exp': 0.,
+        'experiment': "baseline_vaccine_tag"
+    }
+
+    policy_control = Policy_template(lockdown_policy_control_days, lockdown_policy_lower_limits,
+                                     lockdown_policy_upper_limits, testing_policy_control_days,
+                                     testing_policy_lower_limits,
+                                     testing_policy_upper_limits)
+
+    return model, model_case, policy_control
+
+def create_sub_policy(policy_control_times, policy_control_values):
+    policy = {}  # this will hold the policy in format suitable for input to the epidemic model
+    #print("sub_policy: ", policy_control_times, " ", policy_control_values)
+    if policy_control_times == "NA":
+        return "NA"
+    else:
+        for (i, t) in enumerate(policy_control_times):
+            policy[t] = policy_control_values[i]
+
+        return policy
+
+def create_policy(lockdown_policy, testing_policy):
+
+    return Policy(lockdown_policy, testing_policy)
